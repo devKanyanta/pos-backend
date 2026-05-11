dc96f6baef0d93b925ae0058666de433b29f1ba2
Mapalo Kanyanta <ctrophyjoseph@gmail.com>
2026-05-11 20:16:16 +0200

    restured the project structure

---
+++ src/main/java/com/devkanyanta/pos/shared/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.devkanyanta.pos.shared;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
--- src/main/java/com/devkanyanta/pos/testController.java
+++ src/main/java/com/devkanyanta/pos/testController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/test")
     public String test() {
-        return "Hello World!";
+        String name = "Kanyanta Mapalo";
+        return "Hello World this is another test " + name;
     }
 }
