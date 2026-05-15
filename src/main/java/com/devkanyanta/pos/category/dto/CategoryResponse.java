b39bef13811e3352171d2892a1e0d178647a448c
Mapalo Kanyanta <ctrophyjoseph@gmail.com>
2026-05-15 14:12:56 +0200

    Created the Category Module and the product module, also completed the permission management

---
+++ src/main/java/com/devkanyanta/pos/category/dto/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.devkanyanta.pos.category.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CategoryResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
